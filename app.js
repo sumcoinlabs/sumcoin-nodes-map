8dab3c1753eda3870f050066dca371df3238605f
sumcoinlabs <sumcoinindex@gmail.com>
2019-05-16 04:47:21 -0600

    updated app.js

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', async (req, res) => {
     res.render('home', { locations: await locations.getCachedLocations() });
 });
 
-app.listen(8195);
+app.listen(3333);
 
-console.log('listening on port 8195 (proxypass 80)');
+console.log('listening on port 3333 (proxypass 80)');

d8f002a712b2b89906c98a6e8f73c66a8dec08b0
Sumcoin <37975862+sumcoinlabs@users.noreply.github.com>
2023-10-22 00:46:20 -0600

    Update locations.js to geoip-lite

--- locations.js
+++ locations.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
 const CachemanFile = require('cacheman-file');
+const geoip = require('geoip-lite');
 
-const cache = new CachemanFile({tmpDir: '.cache', ttl: 24*3600,});
+const cache = new CachemanFile({ tmpDir: '.cache', ttl: 24 * 3600 });
 
 const getPeers = async () => {
     try {
-		params = {
-        "method": "getpeerinfo",
-		"params": [],
-		"id": "foo"
-      }
+        const params = {
+            "method": "getpeerinfo",
+            "params": [],
+            "id": "foo"
+        }
         const res = await axios.post('http://user:password@127.0.0.1:3332/', params);
-		//console.log(res.data.result);
         if (!res.data.result) throw new Error('Missing peers.');
         console.log(`${res.data.result.length} peers found`);
         return res.data.result;
@@ -21,15 +21,27 @@ const getPeers = async () => {
     }
 };
 
+const formatLocationData = (geoData) => {
+    if (!geoData) return null;
+    return {
+        country_name: geoData.country,
+        region_name: geoData.region,
+        city: geoData.city,
+        latitude: geoData.ll[0],
+        longitude: geoData.ll[1]
+    };
+};
+
 const getLocation = async (ip) => new Promise((resolve) => {
     cache.get(ip, async (err, value) => {
         if (value) {
             return resolve(value);
         }
         try {
-            const res = await axios.get(`http://127.0.0.1:8080/json/${ip}`);
-            cache.set(ip, res.data, 24*3600);
-            resolve(res.data);
+            const geo = geoip.lookup(ip);
+            const formattedGeo = formatLocationData(geo);
+            cache.set(ip, formattedGeo, 24 * 3600);
+            resolve(formattedGeo);
         } catch (e) {
             console.log('Can\'t get location', e);
         }
@@ -38,7 +50,7 @@ const getLocation = async (ip) => new Promise((resolve) => {
 
 const getLocations = async () => {
     return getPeers().then(result => Promise.all(result.map(result => {
-        const [ip, ] = result.addr.split(':');
+        const [ip,] = result.addr.split(':');
         return getLocation(ip);
     })));
 };
